19cb54e8a9969a1cb2e86b12ddf3e0d20fca1c64
Thomas Ardal <thomasardal@gmail.com>
2022-05-04 07:47:52 +0200

    Look for twitter:creator meta element before looking inside the body

--- content.js
+++ content.js
@@ -1,35 +1,35 @@
 function extractTwitterUsernames() {
   var result = [];
-  $("a[href*='twitter']").each(function(link) {
-    var url = this.href;
-
-    var usernameMatch = url.match(/twitter.com\/(?:@){0,1}(\w*)/)
-    if (usernameMatch !== null && !["intent", "search", "share", "home"].includes(usernameMatch[1])) {
-      result.push(usernameMatch[1]);
-      return;
-    }
-
-    var viaMatch = url.match(/via[%20@|=](\w*)/);
-    if (viaMatch !== null) {
-      result.push(viaMatch[1]);
-      return;
-    }
-
-    var screenNameMatch = url.match(/screen_name=(\w*)/);
-    if (screenNameMatch !== null) {
-      result.push(screenNameMatch[1]);
+  $("meta[name='twitter:creator']").each(function(meta) {
+    var content = this.content;
+    if (content !== null && content.startsWith("@")) {
+      result.push(content.substring(1));
       return;
     }
   });
   if (result.length === 0) {
-    $("meta[name='twitter:creator']").each(function(meta) {
-      var content = this.content;
-      if (content !== null && content.startsWith("@")) {
-        result.push(content.substring(1));
+    $("a[href*='twitter']").each(function(link) {
+      var url = this.href;
+  
+      var usernameMatch = url.match(/twitter.com\/(?:@){0,1}(\w*)/)
+      if (usernameMatch !== null && !["intent", "search", "share", "home"].includes(usernameMatch[1])) {
+        result.push(usernameMatch[1]);
+        return;
+      }
+  
+      var viaMatch = url.match(/via[%20@|=](\w*)/);
+      if (viaMatch !== null) {
+        result.push(viaMatch[1]);
+        return;
+      }
+  
+      var screenNameMatch = url.match(/screen_name=(\w*)/);
+      if (screenNameMatch !== null) {
+        result.push(screenNameMatch[1]);
         return;
       }
     });
-  }
+    }
 
   return result.map(function(i) { return i.toLowerCase(); }).filter(function(item, i, ar) { return ar.indexOf(item) === i; });
 }
